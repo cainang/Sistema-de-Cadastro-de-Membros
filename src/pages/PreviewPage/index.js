72df9c9d4cd38932bb8107d3e7ee00e4f7a67cbc
cainang <40045303+cainang@users.noreply.github.com>
2021-03-19 14:07:53 -0300

    Update index.js

--- src/pages/PreviewPage/index.js
+++ src/pages/PreviewPage/index.js
@@ -8,52 +8,21 @@ const PreviewPage = () => {
     const {id} = useParams();
 
     const [data, setData] = useState([]);
-    const [idUser, setIdUser] = useState(null);
-    const [nome, setNome] = useState(null);
-    const [cargo, setCargo] = useState(null);
-    const [congregacao, setCongregacao] = useState(null);
-    const [cpf, setCpf] = useState(null);
-    const [rg, setRg] = useState(null);
-    const [rua, setRua] = useState(null);
-    const [numero, setNumero] = useState(null);
-    const [bairro, setBairro] = useState(null);
-    const [cidade, setCidade] = useState(null);
-    const [naturalidade, setNaturalidade] = useState(null);
-    const [estadoCivil, setEstadoCivil] = useState(null);
-    const [dataBatismo, setDataBatismo] = useState(null);
-    const [dataMembro, setDataMembro] = useState(null);
-    const [dataNascimento, setDataNascimento] = useState(null);
 
     useEffect(async () => {
         await api.get(`membro/${id}`).then(res => {
-            
             setData(res.data);
-        })
+        });
     }, []);
     
-    data.map(res => {
-        setIdUser(res.id);
-        setNome(res.nome);
-        setCargo(res.cargo);
-        setCongregacao(res.congregacao);
-        setCpf(res.cpf);
-        setRg(res.rg);
-        setRua(res.endereco.rua);
-        setNumero(res.endereco.numero);
-        setBairro(res.endereco.bairro);
-        setCidade(res.endereco.cidade);
-        setNaturalidade(res.naturalidade);
-        setEstadoCivil(res.estado_civil);
-        setDataBatismo(res.data_batismo.dateString);
-        setDataMembro(res.data_membro.dateString);
-        setDataNascimento(res.data_nascimento.dateString);
-    })
 
     return (
         <div className="App">
             <header className="App-header">
-                {data.length !== 0 && (
-                    <div id='card'>
+                {data.map(res => {
+
+                    return (
+                        <div id='card'>
                         <div id='carteirinha2'>
                             <div id="cabeca">
                                 <img style={{width: '300px', height: '75px', marginBottom: '17px', marginTop: '17px'}} src={Logo} alt=""/>
@@ -64,33 +33,33 @@ const PreviewPage = () => {
                                     <div id="foto" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', border: '#8B0000 solid 1px'}}></div>
                                     <div>
                                         <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>ID</p>
-                                        <p id='id2'>{idUser}</p>
+                                        <p id='id2'>{res.id}</p>
                                     </div>
                                 </div>
                                 <div id='parte2'>
                                     
                                     <div id="linha1">
                                         <p style={{color: 'black', fontSize: '23px', marginBottom: '5px'}}>Nome</p>
-                                        <p id='nome2'>{nome}</p>
+                                        <p id='nome2'>{res.nome}</p>
                                     </div>
                                     <div id="linha2">
                                         <div id="cpf">
                                             <p style={{color: 'black', fontSize: '23px', marginBottom: '5px'}}>CPF</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{cpf}</p>
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.cpf}</p>
                                         </div>
                                         <div id="rg">
                                             <p style={{color: 'black', fontSize: '23px', marginBottom: '5px'}}>RG</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{rg}</p>
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.rg}</p>
                                         </div>
                                     </div>
                                     <div id="linha3">
                                         <div id="cargo3">
                                             <p style={{color: 'black', fontSize: '23px', marginBottom: '5px'}}>Cargo</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{cargo}</p>
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.cargo}</p>
                                         </div>
                                         <div id="congregacao">
                                             <p style={{color: 'black', fontSize: '23px', marginBottom: '5px'}}>Congregação</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{congregacao}</p>
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '20px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.congregacao}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -102,25 +71,25 @@ const PreviewPage = () => {
                                 <div id="parte1-2" style={{display: 'flex', marginTop: '10px', justifyContent: 'space-around', alignItems: 'center'}}>
                                     <div id="naturalidade">
                                         <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Naturalidade</p>
-                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{naturalidade}</p>  
+                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.naturalidade}</p>  
                                     </div>
                                     <div id="estadocivil">
                                         <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Estado Civil</p>
-                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{estadoCivil}</p>  
+                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.estado_civil}</p>  
                                     </div>
                                 </div>
                                 <div id="parte2-2" style={{display: 'flex', gap: '8px', marginTop: '10px', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center'}}>
                                     <div id="databatismo">
                                         <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Data de Batismo</p>
-                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{dataBatismo}</p>  
+                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.data_batismo.dateString}</p>  
                                     </div>
                                     <div id="datamembro">
                                         <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Data de Aceitação</p>
-                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{dataMembro}</p>  
+                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.data_membro.dateString}</p>  
                                     </div>
                                     <div id="datanascimento">
                                         <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Data de Nascimento</p>
-                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{dataNascimento}</p>  
+                                        <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.data_nascimento.dateString}</p>  
                                     </div>
                                 </div>
                                 <div id="parte3-2" style={{display: 'flex', flexDirection: 'column', gap: '8px', marginTop: '10px', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center'}}>
@@ -130,19 +99,19 @@ const PreviewPage = () => {
                                     <div id="content" style={{display: 'flex', gap: '10px', flexWrap: 'wrap'}}>
                                         <div id="rua">
                                             <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Rua</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{rua}</p>  
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.endereco.rua}</p>  
                                         </div>
                                         <div id="numero">
                                             <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Número</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{numero}</p>  
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.endereco.numero}</p>  
                                         </div>
                                         <div id="bairro">
                                             <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Bairro</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{bairro}</p>  
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.endereco.bairro}</p>  
                                         </div>
                                         <div id="cidade">
                                             <p style={{color: 'black', fontSize: '20px', marginBottom: '5px'}}>Cidade</p>
-                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{cidade}</p>  
+                                            <p style={{marginBottom: '2px', color: 'white', fontSize: '17px', backgroundColor: '#8B0000', borderRadius: '10px', padding: '0px 5px 0px 5px'}}>{res.endereco.cidade}</p>  
                                         </div>
                                     </div>
                                 </div>
@@ -158,8 +127,9 @@ const PreviewPage = () => {
                             </div>
                         </div>
                     </div>
-            
-                )}
+                    )
+                })}
+                    
             </header>
         </div>
     );
